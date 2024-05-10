848ac7f9bf7d1e9e68cd8329cbaa1791bc2ffcdb
kelly <kelvintawiah224@gmail.com>
2024-05-10 19:38:35 +0000

    setup user model and custom JWT

---
+++ auth/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from uuid import uuid4
+from django.utils.translation import gettext as _
+
+
+class CustomBaseUserManager(BaseUserManager):
+    """Custom manager definition for User."""
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        email.lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Model definition for User."""
+
+    id = models.UUIDField(
+        _("id"), primary_key=True, default=uuid4, unique=True, db_index=True
+    )
+    email = models.EmailField(_("email address"), unique=True, db_index=True)
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    inserted_timestamp = models.DateTimeField(_("created at"), auto_now=True)
+    updated_timestamp = models.DateTimeField(_("updated at"), auto_now_add=True)
+    objects = CustomBaseUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    class Meta:
+        """Meta definition for User."""
+
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        """Unicode representation of User."""
+        pass
