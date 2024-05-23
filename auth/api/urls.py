c51ef42de2d540e0340cfe3ac7e1c78c7dccad11
kelly <kelvintawiah224@gmail.com>
2024-05-23 21:32:42 +0000

    setup pytest-drf and added registration test

--- auth/api/urls.py
+++ auth/api/urls.py
@@ -9,9 +9,9 @@ from .views import (
 
 urlpatterns = [
     path("", include("djoser.urls")),
-    path("jwt/create/", CustomTokenObtainPairView.as_view()),
-    path("jwt/refresh/", CustomTokenRefreshView().as_view()),
-    path("jwt/verify/", CustomTokenVerifyView.as_view()),
+    path("jwt/create/", CustomTokenObtainPairView.as_view(), name="jwt-create"),
+    path("jwt/refresh/", CustomTokenRefreshView().as_view(), name="jwt-refresh"),
+    path("jwt/verify/", CustomTokenVerifyView.as_view(), name="jwt-verify"),
     path("o/<str:provider>/", CustomProviderAuthView.as_view()),
     path("logout/", LogoutView.as_view()),
 ]
--- auth/tests.py
+++ auth/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from pytest_drf import (
+    APIViewTest,
+    Returns200,
+    UsesGetMethod,
+    ViewSetTest,
+    UsesPostMethod,
+    Returns201,
+    Returns401,
+    UsesListEndpoint,
+)
+from pytest_drf.util import url_for
+from django.urls import reverse
+import pytest
+from pytest_lambda import lambda_fixture, static_fixture
+from faker import Faker
 
-# Create your tests here.
+fake = Faker()
+
+
+@pytest.mark.django_db
+class TestRegistrationEndpoint(ViewSetTest):
+    # Test for user registration
+    list_url = lambda_fixture(lambda: url_for("user-list"))
+
+    class TestList(UsesGetMethod, UsesListEndpoint, Returns401):
+        def test_get(self, json):
+            expected = {"detail": "Authentication credentials were not provided."}
+            actual = json
+            assert expected == actual
+
+    class TestCreate(UsesPostMethod, UsesListEndpoint, Returns201):
+        password = fake.password()
+        data = static_fixture(
+            value={
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name(),
+                "email": fake.email(),
+                "password": password,
+                "re_password": password,
+            }
+        )
+
+        def test_post_registration_success(self, data, json):
+            expected = data
+            actual = json
+            assert actual.get("id") is not None
+            assert actual.get("email") is not None
+            assert actual.get("last_name") is not None
+            assert actual.get("first_name") is not None
